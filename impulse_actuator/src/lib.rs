6ebfd72a31722ac24fbc6810b3674c2436c29256
Davis Van Sant <davisvansant@users.noreply.github.com>
2021-07-19 16:24:15 -0700

    add new fields, impl, tests

--- impulse_actuator/src/lib.rs
+++ impulse_actuator/src/lib.rs
@@ -1,15 +1,37 @@
-pub struct Actuator {}
+use std::path::PathBuf;
+
+pub struct Actuator {
+    pub firecracker_binary: PathBuf,
+    pub jailer_binary: PathBuf,
+}
 
 impl Actuator {
     pub async fn init() -> Result<Actuator, Box<dyn std::error::Error>> {
-        Ok(Actuator {})
+        let firecracker_binary = PathBuf::from("/usr/bin/firecracker");
+        let jailer_binary = PathBuf::from("/usr/bin/jailer");
+
+        Ok(Actuator {
+            firecracker_binary,
+            jailer_binary,
+        })
     }
 }
 
-// #[cfg(test)]
-// mod tests {
-//     #[test]
-//     fn it_works() {
-//         assert_eq!(2 + 2, 4);
-//     }
-// }
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test(flavor = "multi_thread", worker_threads = 1)]
+    async fn init() -> Result<(), Box<dyn std::error::Error>> {
+        let test_actuator = Actuator::init().await?;
+        assert_eq!(
+            test_actuator.firecracker_binary.to_str().unwrap(),
+            "/usr/bin/firecracker",
+        );
+        assert_eq!(
+            test_actuator.jailer_binary.to_str().unwrap(),
+            "/usr/bin/jailer",
+        );
+        Ok(())
+    }
+}
