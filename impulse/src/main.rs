568ca675d5da232804807b2702bd12db9db17f9f
Davis Van Sant <davisvansant@users.noreply.github.com>
2021-07-19 16:01:44 -0700

    initial bin impl

--- impulse/src/main.rs
+++ impulse/src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use impulse_actuator::Actuator;
+use impulse_interface::Interface;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    Actuator::init().await?;
+    Interface::init().await?;
+    Ok(())
 }
