b5c2ed17a148dc4971509409e50d2d70643f02a7
Davis Van Sant <davisvansant@users.noreply.github.com>
2021-07-19 16:00:00 -0700

    initial struct/impl

--- impulse_actuator/src/lib.rs
+++ impulse_actuator/src/lib.rs
@@ -1,3 +1,11 @@
+pub struct Actuator {}
+
+impl Actuator {
+    pub async fn init() -> Result<Actuator, Box<dyn std::error::Error>> {
+        Ok(Actuator {})
+    }
+}
+
 // #[cfg(test)]
 // mod tests {
 //     #[test]
