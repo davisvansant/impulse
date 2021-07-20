622d18df050f25f1fb641843e9de5bafc1b49879
Davis Van Sant <davisvansant@users.noreply.github.com>
2021-07-19 18:55:24 -0700

    update tests

--- impulse_actuator/src/lib.rs
+++ impulse_actuator/src/lib.rs
@@ -40,18 +40,28 @@ mod tests {
             test_actuator.firecracker_binary.to_str().unwrap(),
             "/usr/bin/firecracker",
         );
+        let test_actuator_fircracker_binary_metadata =
+            fs::metadata(&test_actuator.firecracker_binary).await;
+        assert!(test_actuator_fircracker_binary_metadata.is_err());
         assert_eq!(
             test_actuator.jailer_binary.to_str().unwrap(),
             "/usr/bin/jailer",
         );
+        let test_actuator_jailer_binary_metadata = fs::metadata(&test_actuator.jailer_binary).await;
+        assert!(test_actuator_jailer_binary_metadata.is_err());
         assert_eq!(
             test_actuator.config_base_dir.to_str().unwrap(),
             "/var/lib/impulse/machine"
         );
+        let test_actuator_config_base_dir_metadata =
+            fs::metadata(&test_actuator.config_base_dir).await?;
+        assert!(test_actuator_config_base_dir_metadata.is_dir());
         assert_eq!(
             test_actuator.socket_base.to_str().unwrap(),
             "/tmp/impulse/socket",
         );
+        let test_actuator_socket_base_metadata = fs::metadata(&test_actuator.socket_base).await?;
+        assert!(test_actuator_socket_base_metadata.is_dir());
         Ok(())
     }
 }
