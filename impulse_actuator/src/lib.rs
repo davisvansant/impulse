a031ca347ae43f27ca9400cf8f0b9781943937b2
Davis Van Sant <davisvansant@users.noreply.github.com>
2021-07-20 16:27:50 -0700

    update field name

--- impulse_actuator/src/lib.rs
+++ impulse_actuator/src/lib.rs
@@ -7,7 +7,7 @@ use tokio::process::Command;
 pub struct Actuator {
     pub firecracker_binary: PathBuf,
     pub jailer_binary: PathBuf,
-    pub config_base_dir: PathBuf,
+    pub config_base: PathBuf,
     pub socket_base: PathBuf,
     pub running_pids: Vec<u32>,
 }
@@ -17,8 +17,8 @@ impl Actuator {
         let firecracker_binary = PathBuf::from("/usr/bin/firecracker");
         let jailer_binary = PathBuf::from("/usr/bin/jailer");
 
-        let config_base_dir = PathBuf::from("/var/lib/impulse/machine");
-        fs::create_dir_all(&config_base_dir).await?;
+        let config_base = PathBuf::from("/var/lib/impulse/machine");
+        fs::create_dir_all(&config_base).await?;
 
         let socket_base = PathBuf::from("/tmp/impulse/socket");
         fs::create_dir_all(&socket_base).await?;
@@ -28,7 +28,7 @@ impl Actuator {
         Ok(Actuator {
             firecracker_binary,
             jailer_binary,
-            config_base_dir,
+            config_base,
             socket_base,
             running_pids,
         })
@@ -43,7 +43,7 @@ impl Actuator {
             .as_path()
             .join("socket_name_goes_here.socket");
         let config_file = self
-            .config_base_dir
+            .config_base
             .as_path()
             .join("config_file_name_goes_here.json");
         let command = Command::new(&self.firecracker_binary)
@@ -85,12 +85,11 @@ mod tests {
         let test_actuator_jailer_binary_metadata = fs::metadata(&test_actuator.jailer_binary).await;
         assert!(test_actuator_jailer_binary_metadata.is_err());
         assert_eq!(
-            test_actuator.config_base_dir.to_str().unwrap(),
+            test_actuator.config_base.to_str().unwrap(),
             "/var/lib/impulse/machine"
         );
-        let test_actuator_config_base_dir_metadata =
-            fs::metadata(&test_actuator.config_base_dir).await?;
-        assert!(test_actuator_config_base_dir_metadata.is_dir());
+        let test_actuator_config_base_metadata = fs::metadata(&test_actuator.config_base).await?;
+        assert!(test_actuator_config_base_metadata.is_dir());
         assert_eq!(
             test_actuator.socket_base.to_str().unwrap(),
             "/tmp/impulse/socket",
