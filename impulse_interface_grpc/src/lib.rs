9d3adcbc6b32a7c2552eab1c48631414606104e5
Davis Van Sant <davisvansant@users.noreply.github.com>
2021-07-21 14:42:17 -0700

    initial lib for new workspace member, including build.rs

---
+++ impulse_interface_grpc/src/lib.rs
@@ -0,0 +1,45 @@
+use tonic::{transport::Server, Request, Response, Status};
+
+use impulse::interface_server::{Interface, InterfaceServer};
+use impulse::{
+    Empty, LaunchVmResponse, MicroVm, ShutdownVmResponse, SystemStatusResponse,
+    SystemVersionResponse,
+};
+
+pub mod impulse {
+    tonic::include_proto!("impulse");
+}
+
+#[derive(Default)]
+pub struct Impulse {}
+
+#[tonic::async_trait]
+impl Interface for Impulse {
+    async fn system_status(
+        &self,
+        _request: Request<Empty>,
+    ) -> Result<Response<SystemStatusResponse>, Status> {
+        unimplemented!()
+    }
+
+    async fn system_version(
+        &self,
+        _request: Request<Empty>,
+    ) -> Result<Response<SystemVersionResponse>, Status> {
+        unimplemented!()
+    }
+
+    async fn launch_vm(
+        &self,
+        _request: Request<MicroVm>,
+    ) -> Result<Response<LaunchVmResponse>, Status> {
+        unimplemented!()
+    }
+
+    async fn shutdown_vm(
+        &self,
+        _request: Request<MicroVm>,
+    ) -> Result<Response<ShutdownVmResponse>, Status> {
+        unimplemented!()
+    }
+}
