bc90b060dc45e5c8af33d44f69b6a5559b5ef3f2
Davis Van Sant <davisvansant@users.noreply.github.com>
2021-07-21 10:43:30 -0700

    initial clap usage/layout

--- impulse/src/main.rs
+++ impulse/src/main.rs
@@ -1,9 +1,39 @@
 use impulse_actuator::Actuator;
 use impulse_interface::Interface;
 
+use clap::{App, SubCommand};
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    Actuator::init().await?;
-    Interface::init().await?;
+    let name = env!("CARGO_PKG_NAME");
+    let author = env!("CARGO_PKG_AUTHORS");
+    let version = env!("CARGO_PKG_VERSION");
+
+    let start = SubCommand::with_name("start")
+        .display_order(1)
+        .about("Start");
+    let shutdown = SubCommand::with_name("shutdown")
+        .display_order(2)
+        .about("Shutdown");
+
+    let impulse = App::new(name)
+        .author(author)
+        .version(version)
+        .about("MicroVM runner")
+        .subcommand(start)
+        .subcommand(shutdown)
+        .get_matches();
+
+    match impulse.subcommand_name() {
+        Some("start") => {
+            println!("do starty things");
+
+            Actuator::init().await?;
+            Interface::init().await?;
+        }
+        Some("shutdown") => println!("do shutdowny things"),
+        _ => println!("{}", impulse.usage()),
+    }
+
     Ok(())
 }
