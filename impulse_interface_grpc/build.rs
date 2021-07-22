bdf13f2cd38d65d61adaa915759cd087b8b81752
Davis Van Sant <davisvansant@users.noreply.github.com>
2021-07-22 12:14:56 -0700

    remove old build script

--- impulse_interface_grpc/build.rs
+++
@@ -1,8 +0,0 @@
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    println!("cargo:rerun-if-changed=../proto/impulse/impulse.proto");
-    tonic_build::configure()
-        .build_client(false)
-        .build_server(true)
-        .compile(&["../proto/impulse/impulse.proto"], &["../proto/impulse"])?;
-    Ok(())
-}
