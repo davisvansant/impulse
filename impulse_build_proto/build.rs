7f6adc5146462f4845dcc13b38f50e8a7a75f28a
Davis Van Sant <davisvansant@users.noreply.github.com>
2021-07-22 12:09:48 -0700

    add initial build script

---
+++ impulse_build_proto/build.rs
@@ -0,0 +1,28 @@
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    runner()?;
+    interface()?;
+    Ok(())
+}
+
+fn runner() -> Result<(), Box<dyn std::error::Error>> {
+    println!("cargo:rerun-if-changed=../proto/impulse/impulse_runner.proto");
+    tonic_build::configure()
+        .build_client(false)
+        .build_server(true)
+        .out_dir("../proto/")
+        .compile(
+            &["../proto/impulse/impulse_runner.proto"],
+            &["../proto/impulse"],
+        )?;
+    Ok(())
+}
+
+fn interface() -> Result<(), Box<dyn std::error::Error>> {
+    println!("cargo:rerun-if-changed=../proto/impulse/impulse.proto");
+    tonic_build::configure()
+        .build_client(false)
+        .build_server(true)
+        .out_dir("../proto")
+        .compile(&["../proto/impulse/impulse.proto"], &["../proto/impulse"])?;
+    Ok(())
+}
