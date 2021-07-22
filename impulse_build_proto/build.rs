92204bdc498cd16c08fc049e68ca35589be16721
Davis Van Sant <davisvansant@users.noreply.github.com>
2021-07-22 16:58:00 -0700

    adjust build script to match renamed protos

--- impulse_build_proto/build.rs
+++ impulse_build_proto/build.rs
@@ -1,11 +1,11 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    build_proto("interface")?;
-    build_proto("runner")?;
+    build_proto("external")?;
+    build_proto("internal")?;
     Ok(())
 }
 
 fn build_proto(name: &str) -> Result<(), Box<dyn std::error::Error>> {
-    let proto_file = format!("../proto/impulse/impulse_{}.proto", name);
+    let proto_file = format!("../proto/impulse/impulse_{}_v010.proto", name);
     println!("cargo:rerun-if-changed={}", proto_file);
     tonic_build::configure()
         .build_client(false)
