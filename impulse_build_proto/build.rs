ec07128fb4c2987048e9c1e22dd7d41aa3b72e4a
Davis Van Sant <davisvansant@users.noreply.github.com>
2021-07-23 13:30:46 -0700

    further cleanups/enhancements

--- impulse_build_proto/build.rs
+++ impulse_build_proto/build.rs
@@ -1,15 +1,20 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    build_proto("external")?;
-    build_proto("internal")?;
+    build_proto("external", "v010", false, true)?;
+    build_proto("internal", "v010", false, true)?;
     Ok(())
 }
 
-fn build_proto(name: &str) -> Result<(), Box<dyn std::error::Error>> {
-    let proto_file = format!("../proto/impulse/impulse_{}_v010.proto", name);
+fn build_proto(
+    name: &str,
+    version: &str,
+    client: bool,
+    server: bool,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let proto_file = format!("../proto/impulse/impulse_{}_{}.proto", name, version);
     println!("cargo:rerun-if-changed={}", proto_file);
     tonic_build::configure()
-        .build_client(false)
-        .build_server(true)
+        .build_client(client)
+        .build_server(server)
         .out_dir("../proto")
         .compile(&[proto_file.as_str()], &["../proto/impulse"])?;
     Ok(())
