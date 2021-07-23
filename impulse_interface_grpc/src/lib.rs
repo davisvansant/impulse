2e9153440e4f657e658c1d7e3a4d2ca65920b10b
Davis Van Sant <davisvansant@users.noreply.github.com>
2021-07-23 10:43:32 -0700

    cleanup and export types, mods for use

--- impulse_interface_grpc/src/lib.rs
+++ impulse_interface_grpc/src/lib.rs
@@ -1,45 +1,5 @@
-use tonic::{transport::Server, Request, Response, Status};
+pub use tonic::Streaming;
+pub use tonic::{transport::Server, Request, Response, Status};
 
-use external::interface_server::{Interface, InterfaceServer};
-use external::{
-    Empty, LaunchVmResponse, MicroVm, ShutdownVmResponse, SystemStatusResponse,
-    SystemVersionResponse,
-};
-
-pub mod external {
-    include!("../../proto/impulse.external.v010.rs");
-}
-
-#[derive(Default)]
-pub struct External {}
-
-#[tonic::async_trait]
-impl Interface for External {
-    async fn system_status(
-        &self,
-        _request: Request<Empty>,
-    ) -> Result<Response<SystemStatusResponse>, Status> {
-        unimplemented!()
-    }
-
-    async fn system_version(
-        &self,
-        _request: Request<Empty>,
-    ) -> Result<Response<SystemVersionResponse>, Status> {
-        unimplemented!()
-    }
-
-    async fn launch_vm(
-        &self,
-        _request: Request<MicroVm>,
-    ) -> Result<Response<LaunchVmResponse>, Status> {
-        unimplemented!()
-    }
-
-    async fn shutdown_vm(
-        &self,
-        _request: Request<MicroVm>,
-    ) -> Result<Response<ShutdownVmResponse>, Status> {
-        unimplemented!()
-    }
-}
+pub mod external;
+pub mod internal;
