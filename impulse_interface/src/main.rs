589eed7aecc6258ec747e80b51baa37630b09550
Davis Van Sant <davisvansant@users.noreply.github.com>
2021-07-24 12:15:43 -0700

    minor adjustments, include new signal

--- impulse_interface/src/main.rs
+++ impulse_interface/src/main.rs
@@ -4,7 +4,13 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let external_interface = impulse_interface_grpc::external::External::default();
     let internal_interface = impulse_interface_grpc::internal::Internal::default();
 
-    println!("starting service on {}", run_address);
+    println!(": : i m p u l s e _ i n t e r f a c e > Launching system on {}", run_address);
+
+    let ctrl_c = async move {
+        println!(": : i m p u l s e _ i n t e r f a c e > Running...");
+        tokio::signal::ctrl_c().await.unwrap();
+        println!(": : i m p u l s e _ i n t e r f a c e > Shutting down...");
+    };
 
     impulse_interface_grpc::Server::builder()
         .add_service(impulse_interface_grpc::external::InterfaceServer::new(
@@ -13,7 +19,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .add_service(impulse_interface_grpc::internal::InterfaceServer::new(
             internal_interface,
         ))
-        .serve(run_address)
+        .serve_with_shutdown(run_address, ctrl_c)
         .await?;
 
     Ok(())
