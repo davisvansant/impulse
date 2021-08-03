fbee148fdce288fe76e31e68ad95f0e0235c76b8
Davis Van Sant <davisvansant@users.noreply.github.com>
2021-08-03 09:57:45 -0700

    adjust to new layout/structure

---
+++ deps/impulse_prost/src/lib.rs
@@ -0,0 +1,3 @@
+// wraps the tonic and prost crates and enables use of features for crates in this workspace
+
+pub use prost::*;
---
+++ deps/impulse_tonic/src/lib.rs
@@ -0,0 +1,3 @@
+// wraps the tonic crate and enables use of features for crates in this workspace
+
+pub use tonic::*;
