0196ab29460ad66e4585cde14bc6040d65100190
Davis Van Sant <davisvansant@users.noreply.github.com>
2021-08-05 14:46:39 -0700

    add more details on init

--- impulse_actuator/src/main.rs
+++ impulse_actuator/src/main.rs
@@ -7,12 +7,31 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // });
 
     let endpoint = "http://[::1]:1284";
-    let mut internal_client = impulse_actuator_internal::Internal::init(endpoint).await?;
-    let mut engine = impulse_actuator_engine::Engine::init().await?;
+    println!(
+        ":: i m p u l s e _ a c t u a t o r > connecting | {}",
+        &endpoint,
+    );
 
-    internal_client.register().await?;
+    let mut internal_client = impulse_actuator_internal::Internal::init(endpoint).await?;
+    println!(
+        ":: i m p u l s e _ a c t u a t o r >  node id | {}",
+        &internal_client.node_id,
+    );
+
+    let mut engine = impulse_actuator_engine::Engine::init().await?;
+    println!(
+        ":: i m p u l s e _ a c t u a t o r > engine active | {}",
+        &engine.active,
+    );
+
+    let register = internal_client.register().await?;
+    println!(
+        ":: i m p u l s e _ a c t u a t o r > endpoint system id | {}",
+        &register.get_ref().system_id,
+    );
 
     let mut controller = internal_client.controller().await?;
+    println!(":: i m p u l s e _ a c t u a t o r > awaiting tasks |");
 
     while let Some(task) = controller.get_mut().message().await? {
         match task.action {
