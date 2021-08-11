905e184665da63a82a66835486f0c59b95ba3212
Davis Van Sant <davisvansant@users.noreply.github.com>
2021-08-11 14:44:55 -0700

    adjust to use type

--- impulse_interface/src/main.rs
+++ impulse_interface/src/main.rs
@@ -1,14 +1,18 @@
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let run_address = "[::1]:1284".parse().unwrap();
+    let ipv4_addr = std::net::Ipv4Addr::new(0, 0, 0, 0);
+    let port = 1284;
+    let socket_addr = std::net::SocketAddr::new(std::net::IpAddr::V4(ipv4_addr), port);
+
     let (tx, _) = tokio::sync::broadcast::channel(1);
     let sender_clone = tx.clone();
+
     let external_interface = impulse_interface_external::External::init(tx).await?;
     let internal_interface = impulse_interface_internal::Internal::init(sender_clone).await?;
 
     println!(
         ":: i m p u l s e _ i n t e r f a c e > Launching system on {}",
-        run_address,
+        &socket_addr,
     );
 
     let ctrl_c = async move {
@@ -24,7 +28,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .add_service(impulse_interface_internal::InterfaceServer::new(
             internal_interface,
         ))
-        .serve_with_shutdown(run_address, ctrl_c)
+        .serve_with_shutdown(socket_addr, ctrl_c)
         .await?;
 
     Ok(())
