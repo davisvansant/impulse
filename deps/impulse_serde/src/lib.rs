e3eb773460bcc3b66fea66ada5839ca790669949
Davis Van Sant <davisvansant@users.noreply.github.com>
2021-08-12 17:00:59 -0700

    add new deps

---
+++ deps/impulse_serde/src/lib.rs
@@ -0,0 +1,3 @@
+// wraps the serde crate and enables use of features for crates in this workspace
+
+pub use serde::*;
---
+++ deps/impulse_serde_json/src/lib.rs
@@ -0,0 +1,3 @@
+// wraps the serde_json crate and enables use of features for crates in this workspace
+
+pub use serde_json::*;
