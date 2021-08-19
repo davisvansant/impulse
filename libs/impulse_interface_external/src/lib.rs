e75816ff1c6071371a91d084a280a028db0651b4
Davis Van Sant <davisvansant@users.noreply.github.com>
2021-08-19 11:22:12 -0700

    add new struct field, add impl to accept channel, updated tests

--- libs/impulse_interface_external/src/lib.rs
+++ libs/impulse_interface_external/src/lib.rs
@@ -17,10 +17,14 @@ pub struct External {
     status: String,
     pub version: String,
     sender: Sender<u8>,
+    sender_clone: Sender<String>,
 }
 
 impl External {
-    pub async fn init(sender: Sender<u8>) -> Result<External, Box<dyn std::error::Error>> {
+    pub async fn init(
+        sender: Sender<u8>,
+        sender_clone: Sender<String>,
+    ) -> Result<External, Box<dyn std::error::Error>> {
         let status = String::from("Running!");
         let version = String::from("v0.1.0");
 
@@ -28,6 +32,7 @@ impl External {
             status,
             version,
             sender,
+            sender_clone,
         })
     }
 }
@@ -78,13 +83,22 @@ impl Interface for External {
             );
         }
 
-        let launch_vm = LaunchVmResponse {
-            launched: true,
-            details: String::from("vm has started!"),
-        };
-        let response = Response::new(launch_vm);
+        let mut receiver = self.sender_clone.subscribe();
 
-        Ok(response)
+        if let Ok(message) = receiver.recv().await {
+            let launch_vm = LaunchVmResponse {
+                launched: true,
+                details: message,
+            };
+
+            let response = Response::new(launch_vm);
+
+            Ok(response)
+        } else {
+            let message = String::from("Something went wrong!");
+            let status = Status::new(tonic::Code::NotFound, message);
+            Err(status)
+        }
     }
 
     async fn shutdown_vm(
@@ -121,7 +135,9 @@ mod tests {
     #[tokio::test(flavor = "multi_thread")]
     async fn init() -> Result<(), Box<dyn std::error::Error>> {
         let (test_tx, _rx) = tokio::sync::broadcast::channel(1);
-        let test_external = External::init(test_tx).await?;
+        let (test_response_sender, _) = tokio::sync::broadcast::channel(1);
+        let test_response_sender_clone = test_response_sender.clone();
+        let test_external = External::init(test_tx, test_response_sender_clone).await?;
         assert_eq!(test_external.status.as_str(), "Running!");
         assert_eq!(test_external.version.as_str(), "v0.1.0");
         Ok(())
@@ -130,7 +146,9 @@ mod tests {
     #[tokio::test(flavor = "multi_thread")]
     async fn system_status() -> Result<(), Box<dyn std::error::Error>> {
         let (test_tx, _rx) = tokio::sync::broadcast::channel(1);
-        let test_external = External::init(test_tx).await?;
+        let (test_response_sender, _) = tokio::sync::broadcast::channel(1);
+        let test_response_sender_clone = test_response_sender.clone();
+        let test_external = External::init(test_tx, test_response_sender_clone).await?;
         let test_request = Request::new(Empty {});
         let test_external_system_status = test_external.system_status(test_request).await?;
         assert_eq!(
@@ -143,7 +161,9 @@ mod tests {
     #[tokio::test(flavor = "multi_thread")]
     async fn system_version() -> Result<(), Box<dyn std::error::Error>> {
         let (test_tx, _rx) = tokio::sync::broadcast::channel(1);
-        let test_external = External::init(test_tx).await?;
+        let (test_response_sender, _) = tokio::sync::broadcast::channel(1);
+        let test_response_sender_clone = test_response_sender.clone();
+        let test_external = External::init(test_tx, test_response_sender_clone).await?;
         let test_request = Request::new(Empty {});
         let test_external_system_version = test_external.system_version(test_request).await?;
         assert_eq!(
@@ -156,16 +176,28 @@ mod tests {
     #[tokio::test(flavor = "multi_thread")]
     async fn launch_vm_response() -> Result<(), Box<dyn std::error::Error>> {
         let (test_tx, _rx) = tokio::sync::broadcast::channel(1);
-        let test_external = External::init(test_tx).await?;
+        let (test_response_sender, _test_response_rx) = tokio::sync::broadcast::channel(1);
+        let test_response_sender_clone = test_response_sender.clone();
+        drop(_test_response_rx);
+        let test_external = External::init(test_tx, test_response_sender_clone).await?;
         let test_request = Request::new(MicroVm {
             name: String::from("tester"),
         });
-        let test_external_launch_vm = test_external.launch_vm(test_request).await?;
-        assert!(test_external_launch_vm.get_ref().launched);
-        assert_eq!(
-            test_external_launch_vm.get_ref().details.as_str(),
-            "vm has started!"
-        );
+        let test_result = tokio::spawn(async move {
+            let test_external_launch_vm = test_external.launch_vm(test_request).await.unwrap();
+            assert!(test_external_launch_vm.get_ref().launched);
+            assert_eq!(
+                test_external_launch_vm.get_ref().details.as_str(),
+                "test_uuid"
+            );
+        });
+        if test_response_sender.receiver_count() == 0 {
+            tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
+        }
+        test_response_sender
+            .send(String::from("test_uuid"))
+            .expect("could not send!");
+        assert!(test_result.await.is_ok());
         Ok(())
     }
 
@@ -191,7 +223,9 @@ mod tests {
     #[tokio::test(flavor = "multi_thread")]
     async fn shutdown_vm_response() -> Result<(), Box<dyn std::error::Error>> {
         let (test_tx, _rx) = tokio::sync::broadcast::channel(1);
-        let test_external = External::init(test_tx).await?;
+        let (test_response_sender, _) = tokio::sync::broadcast::channel(1);
+        let test_response_sender_clone = test_response_sender.clone();
+        let test_external = External::init(test_tx, test_response_sender_clone).await?;
         let test_request = Request::new(MicroVm {
             name: String::from("tester"),
         });
@@ -207,7 +241,9 @@ mod tests {
     #[tokio::test(flavor = "multi_thread")]
     async fn shutdown_vm_status() -> Result<(), Box<dyn std::error::Error>> {
         let (test_tx, _rx) = tokio::sync::broadcast::channel(1);
-        let test_external = External::init(test_tx).await?;
+        let (test_response_sender, _) = tokio::sync::broadcast::channel(1);
+        let test_response_sender_clone = test_response_sender.clone();
+        let test_external = External::init(test_tx, test_response_sender_clone).await?;
         let test_request = Request::new(MicroVm {
             name: String::from("not tester"),
         });
