3febe2efb8c953236270a613205b39f41d89e49e
Davis Van Sant <davisvansant@users.noreply.github.com>
2021-08-23 15:41:20 -0700

    cleanup, add new use paths

--- system/src/bin/impulse_actuator.rs
+++ system/src/bin/impulse_actuator.rs
@@ -1,3 +1,7 @@
+use system::actuator_client::Internal;
+use system::actuator_engine::Engine;
+use system::IMPULSE_ACTUATOR;
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // let ctrl_c = tokio::spawn(async move {
@@ -7,42 +11,38 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // });
 
     let endpoint = "http://[::1]:1284";
+    println!("{} connecting | {}", IMPULSE_ACTUATOR, &endpoint);
+
+    let mut internal_client = Internal::init(endpoint).await?;
     println!(
-        ":: i m p u l s e _ a c t u a t o r > connecting | {}",
-        &endpoint,
+        "{} node id | {}",
+        IMPULSE_ACTUATOR, &internal_client.node_id,
     );
 
-    let mut internal_client = impulse_actuator_internal::Internal::init(endpoint).await?;
-    println!(
-        ":: i m p u l s e _ a c t u a t o r > node id | {}",
-        &internal_client.node_id,
-    );
-
-    let mut engine = impulse_actuator_engine::Engine::init().await?;
-    println!(
-        ":: i m p u l s e _ a c t u a t o r > engine active | {}",
-        &engine.active,
-    );
+    let mut engine = Engine::init().await?;
+    println!("{} engine active | {}", IMPULSE_ACTUATOR, &engine.active);
 
     let register = internal_client.register().await?;
     println!(
-        ":: i m p u l s e _ a c t u a t o r > endpoint system id | {}",
+        "{} endpoint system id | {}",
+        IMPULSE_ACTUATOR,
         &register.get_ref().system_id,
     );
 
     let mut controller = internal_client.controller().await?;
-    println!(":: i m p u l s e _ a c t u a t o r > awaiting tasks . . .");
+    println!("{} awaiting tasks . . .", IMPULSE_ACTUATOR);
 
     while let Some(task) = controller.get_mut().message().await? {
         match task.action {
             1 => {
                 println!("start a vm {:?}", task);
                 engine.launch_vm(&task.id).await?;
-                internal_client.result(&task.id).await?;
+                internal_client.launch_result(&task.id).await?;
             }
             2 => {
                 println!("shutdown a vm {:?}", task);
                 engine.shutdown_vm(&task.id).await?;
+                internal_client.shutdown_result(&task.id).await?;
             }
             _ => (),
         }
