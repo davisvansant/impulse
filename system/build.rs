06f4578fcc72ad936cb2595867b9d2d212e1d3f5
Davis Van Sant <davisvansant@users.noreply.github.com>
2021-08-23 15:33:36 -0700

    adjust build paths, add new fn() for shared

--- system/build.rs
+++ system/build.rs
@@ -2,6 +2,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("cargo:rerun-if-changed=build.rs");
     build_proto("external", "v010", true, true)?;
     build_proto("internal", "v010", true, true)?;
+    build_proto("shared", "v010", true, true)?;
     Ok(())
 }
 
@@ -11,12 +12,12 @@ fn build_proto(
     client: bool,
     server: bool,
 ) -> Result<(), Box<dyn std::error::Error>> {
-    let proto_file = format!("../../proto/impulse/impulse_{}_{}.proto", name, version);
+    let proto_file = format!("../proto/impulse/impulse_{}_{}.proto", name, version);
     println!("cargo:rerun-if-changed={}", proto_file);
     tonic_build::configure()
         .build_client(client)
         .build_server(server)
-        .out_dir("../../proto")
-        .compile(&[proto_file.as_str()], &["../../proto/impulse"])?;
+        .out_dir("../proto")
+        .compile(&[proto_file.as_str()], &["../proto/impulse"])?;
     Ok(())
 }
